22598ed7e78341cd7fa7a3063098a6ba377ac5e0
Avinash Kalwale <avinashkalwale12@gmail.com>
2023-03-18 08:10:56 +0530

    signup added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from "react-router-dom";
 
 import SignIn from './components/SignIn_SignUp/SignIn';
+import SignUp from './components/SignIn_SignUp/SignUp';
 function App() {
   return (
     <div>
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<SignIn />} >
           {/* <Route path=":email" element={<Header/>}/> */}
         </Route>
+        <Route path="/signUp" element={<SignUp />} />
         </Routes>  
     </div>
 
